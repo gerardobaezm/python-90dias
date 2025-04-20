3ce5a212b19e859884db1b90e3f46b74446705e1
GJBM <gerardobaezm@gmail.com>
2025-04-20 17:28:15 +0200

    Dia 1: Variables en Python

---
+++ dia_01_variables/main.py
@@ -0,0 +1,29 @@
+# 👋 Hello Python!
+name = "Gerardo"
+age = 33
+height = 1.75
+is_student = True
+
+# 🖨️ Print variables
+print(f"My name is {name}")
+print(f"I am {age} years old")
+print(f"My height is {height} meters")
+print(f"Am I a student? {is_student}")
+
+# 🔍 Show types
+print(type(name))      # <class 'str'>
+print(type(age))       # <class 'int'>
+print(type(height))    # <class 'float'>
+print(type(is_student))# <class 'bool'>
+
+print(f"hola, soy {name}, tengo {age}, mido {height} metros y ¿soy estudiante? {is_student}")
+
+# Mi ejercicio
+
+name2 = "Juan"
+age2 = 34
+height2 = 1.70
+city = "Waterloo"
+
+print(f"Soy {name2}, tengo {age2}, vivo en {city} y mido {height2}m")
+print(type(city))
